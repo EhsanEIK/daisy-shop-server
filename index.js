1dc4e36a0a958e524196cb8c4c059fca6605ae6c
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-31 22:42:25 +0600

    added DELETE method to delete a product from database

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,6 +29,14 @@ const run = async () => {
         app.post('/products', async (req, res) => {
             const product = req.body;
             const result = await productsCollection.insertOne(product);
+            res.send(result);
+        })
+
+        // DELETE method
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
             res.send(result);
         })
 
