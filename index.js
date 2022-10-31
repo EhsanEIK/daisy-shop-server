9e75b0229197165f470ac6f1458c525182e38ba2
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-31 21:03:50 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// dbuser1:5B2PYCYai5q6XLjM
+const dbUser = process.env.REACT_APP_dbUser;
+const dbPassword = process.env.REACT_APP_dbPassword;
+
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('db connected')
+    client.close();
+});
 
 app.get('/', (req, res) => {
     res.send('Daisy Shop server is running');
